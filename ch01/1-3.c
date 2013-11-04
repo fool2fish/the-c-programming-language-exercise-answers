92357938350d8a05fefb6fef25cf8b4b1335245a
fool2fish <fool2fish@gmail.com>
2013-11-04 19:52:05 +0800

    Add 1-3 to 1-5.

---
+++ ch01/1-3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int fahr2celsius(int degree) {
+	return 5 * (degree - 32) / 9;
+}
+
+int printFahrList(int start, int end, int step, int withHead) {
+	if (start <= end && step < 0 || start >= end && step > 0 || step == 0) {
+		printf("params error!\n");
+		return 1;
+	}
+
+	if (withHead) {
+		printf("fahr\tcelsius\n");
+	}
+
+	int i = start;
+	while (start <= end ? i <= end : i >= end ) {
+		printf("%d\t%d\n", i, fahr2celsius(i));
+		i += step;
+	}
+}
+
+int main() {
+	printFahrList(0, 300, 20, 1);
+}
---
+++ ch01/1-4.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int celsius2fahr(int degree) {
+	return degree * 9 / 5 + 32;
+}
+
+int printFahrList(int start, int end, int step, int withHead) {
+	if (start <= end && step < 0 || start >= end && step > 0 || step == 0) {
+		printf("params error!\n");
+		return 1;
+	}
+
+	if (withHead) {
+		printf("celsius\tfahr\n");
+	}
+
+	int i = start;
+	while (start <= end ? i <= end : i >= end ) {
+		printf("%d\t%d\n", i, celsius2fahr(i));
+		i += step;
+	}
+}
+
+int main() {
+	printFahrList(-17, 148, 11, 1);
+}

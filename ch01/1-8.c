226fd2413e16b647e3043ef392aa6393fe5496fb
fool2fish <fool2fish@gmail.com>
2013-11-07 10:21:30 +0800

    Add answer 1-8.

---
+++ ch01/1-8.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int space = 0;
+int tab = 0;
+int newline = 0;
+
+int main() {
+	int c;
+	while((c = getchar()) != EOF) {
+		switch (c) {
+		case ' ':
+			space++;
+			break;
+		case '\t':
+			tab++;
+			break;
+		case '\n':
+			newline++;
+			break;
+		default:
+			break;
+		}
+	}
+	printf("space:%d, tab:%d, newline:%d\n", space, tab, newline);
+}
+

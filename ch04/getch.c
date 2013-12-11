ace961b19495efac29221f17c1239bc46862e47f
fool2fish <fool2fish@gmail.com>
2013-12-11 19:38:16 +0800

    Add getch() and ungetch().

---
+++ ch04/getch.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#define BUFSIZE 100
+
+char buf[BUFSIZE];
+int p = 0;
+
+int getch() {
+  return p > 0 ? buf[p--] : getchar();
+}
+
+int ungetch(char c) {
+  if (p >= BUFSIZE) {
+    printf("error: buf is full.");
+    return 0;
+  } else {
+    buf[p++] = c;
+    return 1;
+  }
+}
---
+++ ch04/getch.h
@@ -0,0 +1,5 @@
+#ifndef _GETCH_H_
+#define _GETCH_H_
+int getch();
+int ungetch(char c);
+#endif

e16bf3595b5bbb3b2523dbbf5cce5de8cc6cbd7d
fool2fish <fool2fish@gmail.com>
2013-12-25 22:51:33 +0800

    Add 5-13 answer.

---
+++ ch05/5-13-tail/input.txt
@@ -0,0 +1,12 @@
+line1
+line2
+line3
+line4
+line5
+line6
+line7
+line8
+line9
+line10
+line11
+line12
---
+++ ch05/5-13-tail/tail.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAXLINE 1000
+
+void tail(int n);
+static int getline2(char *s, int len);
+static void strcpy2(char *from, char *to);
+
+int main(int argc, char *argv[]) {
+  int n  = 10;
+  while (--argc > 0) {
+    char *arg = *++argv;
+    if (*arg == '-') {
+      n = atoi(++arg);
+    }
+  }
+  tail(n);
+}
+
+void tail(int n) {
+  int count = 0;
+  char *lines[n];
+
+  char s[MAXLINE];
+  int w;
+  char *p;
+
+  while (w = getline2(s, MAXLINE)) {
+    p = malloc(w);
+
+    if (p == NULL) {
+      printf("Memory allocation error.");
+      return;
+
+    } else {
+      strcpy2(s, p);
+      if (count >= n) {
+        free(lines[count % n]);
+      }
+      lines[count % n] = p;
+      count++;
+    }
+  }
+
+  int start = 0;
+  int end = count;
+  if (count >= n) {
+    start = count;
+    end = count + n;
+  }
+  printf("---->\n");
+  for (; start < end; start++) {
+    printf("%s", lines[start % n]);
+  }
+  printf("<----\n");
+}
+
+static int getline2(char *s, int len) {
+  static char c;
+  if (c == EOF) {
+    return 0;
+  }
+
+  int i = 0;
+  while ((c = getchar()) != '\n' && c != EOF && i < len - 1) {
+    *(s + i++) = c;
+  }
+  if (c == '\n') {
+    *(s + i++) = c;
+  }
+  *(s + i) = '\0';
+  return i;
+}
+
+static void strcpy2(char *from, char *to) {
+  while (*from != '\0') {
+    *to++ = *from++;
+  }
+  *to = '\0';
+}
+

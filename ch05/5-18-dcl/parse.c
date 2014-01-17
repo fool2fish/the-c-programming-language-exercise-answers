bd9dd2cca46ec6f8ffce21f1ae7d8025ff74302c
fool2fish <fool2fish@gmail.com>
2014-01-17 09:04:44 +0800

    Finish parser. 2 points to improve: use lookahead() to avoid ungettoken; create struct node to optimize sdt output.

--- ch05/5-18-dcl/parse.c
+++ ch05/5-18-dcl/parse.c
@@ -8,8 +8,14 @@ char specifiers[MAXTOKEN];
 
 static void declaration_specifiers();
 static void declarator();
-static void pointers();
+static int pointers();
 static void direct_declarator();
+static void direct_declarator_simple();
+static void direct_declarator_arr_fn();
+static void director_declarator_postfix();
+static void printError(char *s);
+static void expectNext(int c, char *s);
+
 
 void parse() {
   declaration_specifiers();
@@ -29,21 +35,99 @@ static void declarator() {
   gettoken();
   if (tokentype == '*') {
     ungettoken();
-    pointers();
+    int i = pointers();
     direct_declarator();
+    while(--i >= 0) {
+      printf("pointer to ");
+    }
   } else {
     ungettoken();
     direct_declarator();
   }
 }
 
-static void pointers() {
+static int pointers() {
+  int i = 0;
   while (gettoken(), tokentype == '*') {
-    printf("pointer to ");
+    i++;
   }
   ungettoken();
+  return i;
 }
 
 static void direct_declarator() {
-
+  direct_declarator_simple();
+  direct_declarator_arr_fn();
 }
+
+static void direct_declarator_simple() {
+  char s[] = "direct_declarator_simple";
+  gettoken();
+  if (tokentype == '(') {
+    declarator();
+    expectNext(')', s);
+  } else if (tokentype == IDENTIFIER) {
+    printf("%s: ", tokenval);
+  } else {
+    printError(s);
+  }
+}
+
+static void direct_declarator_arr_fn() {
+  gettoken();
+  if (tokentype == '[' || tokentype == '(') {
+    ungettoken();
+    director_declarator_postfix();
+    direct_declarator_arr_fn();
+  } else {
+    // epsilon
+    ungettoken();
+  }
+}
+
+static void director_declarator_postfix() {
+  char s[] ="director_declarator_postfix";
+  gettoken();
+  if (tokentype == '[') {
+    gettoken();
+    if (tokentype == ']') {
+      printf("array[] of ");
+    } else if (tokentype == DIGITS) {
+      printf("array[%s] of ", tokenval);
+      expectNext(']', s);
+    } else {
+      printError(s);
+    }
+  } else if (tokentype == '(') {
+    while (gettoken(), tokentype != ')') {
+    }
+    printf("function() returning ");
+  } else {
+    printError(s);
+  }
+}
+
+
+static void printError(char *fn) {
+  printf("\nParse error: %s\n  tokentype: %d,", fn, tokentype);
+  if (ILLEGAL <= tokentype && tokentype <= DIGITS) {
+    printf(" tokenval: %s,", tokenval);
+  } else {
+    printf(" tokenval: %c,", tokentype);
+  }
+  printf(" line: %d, col: %d\n", tokenline, tokencol);
+}
+
+static void expectNext(int c, char *fn) {
+  gettoken();
+  if (tokentype != c) {
+    printError(fn);
+    ungettoken();
+  }
+}
+
+
+
+
+
+
